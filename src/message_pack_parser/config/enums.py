a6a0a78cb71ba6a7d5e09d330ca9458f4fa7a4d3
anovio1 <adocsgo@gmail.com>
2025-06-21 16:42:50 -0400

    refactor: added enum-to-int transformation to output contract and serializer

--- src/message_pack_parser/config/enums.py
+++ src/message_pack_parser/config/enums.py
@@ -1,5 +1,6 @@
 """This module contains all Enum definitions for the application."""
 from enum import IntEnum
+from typing import Dict, Type
 
 class CommandsEnum(IntEnum):
     BUILD = 1; ATTACK = 2; CAPTURE = 3; FIGHT = 4; GUARD = 5; LOAD_UNITS = 6; MANUAL_FIRE = 7; MOVE = 8; PATROL = 9; RECLAIM = 10; REPAIR = 11; RESURRECT = 12; STOP = 13; UNLOAD_UNITS = 14; WAIT = 15
@@ -12,3 +13,11 @@ class UnitEconomyEventsEnum(IntEnum):
 
 class UnitEventsEnum(IntEnum):
     CREATED = 1; FINISHED = 2; DESTROYED = 3; GIVEN = 4; TAKEN = 5
+
+ENUM_REGISTRY: Dict[str, Type[IntEnum]] = {
+    "UnitEventsEnum": UnitEventsEnum,
+    "CommandsEnum": CommandsEnum,
+    "ConstructionActionsEnum": ConstructionActionsEnum,
+    "UnitEconomyEventsEnum": UnitEconomyEventsEnum,
+    # Add any other enums that will need this string-to-int mapping in the future.
+}
